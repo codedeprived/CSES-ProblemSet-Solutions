6fdb493f118d8b3630e1c0d473e22dc6662c92b4
codedeprived <smanmohan735@gmail.com>
2024-10-13 21:54:48 +0530

    session 2

--- IntroductoryProblem/IncreasingArray.cpp
+++ IntroductoryProblem/IncreasingArray.cpp
@@ -5,5 +5,25 @@ using namespace std;
 
 int main()
 {
-  
+   ll n; 
+   cin>>n;
+   ll op = 0; 
+   ll prev;
+   for(int i = 0;  i<n ; i++)
+   {
+        ll temp ;
+        cin>>temp;
+        if(i!= 0 && prev > temp)
+        {
+            op += prev - temp;
+            prev = temp + (prev-temp);
+        }
+        else
+        {
+            prev = temp;
+        }
+        
+   }
+
+   cout<<op;
 }
