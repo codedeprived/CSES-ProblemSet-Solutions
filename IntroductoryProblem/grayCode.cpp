a5b0ce699331eedf824e63af3b44b6322109266e
codedeprived <smanmohan735@gmail.com>
2024-10-27 21:30:09 +0530

    :)

---
+++ IntroductoryProblem/grayCode.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#define ll long long
+#define ld long double
+// Input will be 1 - 16 integer
+// if n = 2 , generate 00 10 11 01
+// if n = 3 , generate 000 100 010 001 110 101 011 111
+// if n = 4 , generate 0000 1000 0100 0010 0001 1100 1010 1001 0101 0011 0110 1110 0111 1011 1101 1111
+
+
+int main()
+{
+  
+}
